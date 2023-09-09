f795cdbd92f3f28a3e57925baed40798459e5f4d
aosnotes77 <102222046+aosnotes77@users.noreply.github.com>
2023-09-09 10:52:33 -0400

    Update data-migrate-ec2.tf

--- data-migrate-ec2.tf
+++ data-migrate-ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "data_migrate_ec2" {
   vpc_security_group_ids = 
   iam_instance_profile   = 
 
-  user_data = base64encode(templatefile("${path.module}/<enter the name of your script here>.tpl", {
+  user_data = base64encode(templatefile("${path.module}/<enter the name of your script here>", {
     RDS_ENDPOINT = 
     RDS_DB_NAME  = 
     USERNAME     = 
